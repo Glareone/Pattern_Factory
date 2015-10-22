f7aa5a4981c210e0a147c85dfe649b83e8b42c91
Glareone <enlarius@gmail.com>
2015-10-22 12:57:09 +0300

    initial creation

---
+++ Factory/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Factory
+{
+    abstract class Product { }
+
+    class ConcreteProductA : Product
+    {
+        public string GetType() { return "ConcreteProductA"; }
+    }
+
+    class ConcreteProductB : Product
+    {
+        public string GetType() { return "ConcreteProductB"; }
+    }
+
+    abstract class Creator
+    {
+        public abstract Product FactoryMethod();
+    }
+
+    class ConcreteCreatorA : Creator
+    {
+        public override Product FactoryMethod() { return new ConcreteProductA(); }
+    }
+
+    class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod() { return new ConcreteProductB(); }
+    }
+
+    public class MainApp
+    {
+        public static void Main()
+        {
+            // an array of creators
+            Creator[] creators = { new ConcreteCreatorA(), new ConcreteCreatorB() };
+            // iterate over creators and create products
+            foreach (Creator creator in creators)
+            {
+                Product product = creator.FactoryMethod();
+                Console.WriteLine("Created {0}", product.GetType());
+            }
+            // Wait for user
+            Console.Read();
+        }
+    }
+}
